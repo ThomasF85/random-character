73b4aed6d6535aa85db54c13d05c1cdc5c85745c
ThomasF85 <thomas.foeldi@gmail.com>
2023-08-04 10:49:38 +0200

    add api route with random character and frontend

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR("/api/random-character");
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  console.log(data);
+  return (
+    <>
+      <h1>Hello {data.firstName}</h1>
+      <p>
+        Homepage of {data.firstName} {data.lastName}
+      </p>
+    </>
+  );
 }
